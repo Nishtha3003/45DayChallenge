6e55bd43543cf61d5db2acc9f809d9821409504b
Nishtha <nistha@terrafin.tech>
2023-06-29 23:13:35 +0530

    new commit

--- src/JAVA/AlgoPrep151/ArrayRotation.java
+++ src/JAVA/AlgoPrep151/ArrayRotation.java
@@ -15,6 +15,41 @@ public class ArrayRotation {
                 System.out.print(nums[i] + " ");
             }
         }
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0 , nums.length - 1);
+        reverse(nums, 0 , k-1);
+        reverse(nums,k,nums.length - 1);
+    }
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0 ; i< nums.length ; i++ )
+        {
+            sum += nums[i];
+            if(sum > maxSum)
+            {
+                maxSum = sum;
+            }
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+    public static void reverse(int nums[], int start, int end)
+    {
+        while(start<end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
        public static  void  main(String[] args)
        {
            ArrayRotation ar = new ArrayRotation();
